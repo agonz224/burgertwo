38d3edeaa660702424691d0090a6c77ea5973fb5
Daniel Gonzalez <Daniel Gonzalez>
2017-05-05 21:14:06 -0400

    sequelized

--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,30 @@
-// Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
-
-var burgers = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
-  }
+module.exports = function(sequelize, DataTypes) {
+  var burgers = sequelize.define("burgers", {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      validate: {
+        len: [1]
+      }
+    },
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1]
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+      len: [1]
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      len: [1]
+    }
+  });
+  return burgers;
 };
-
-// Export the database functions for the controller (catsController.js).
-module.exports = burgers;
