1ed8fc1e488785ab478b647ce46c6495bbbf3d6f
Daniel Gonzalez <Daniel Gonzalez>
2017-05-05 21:07:12 -0400

    server issues

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var PORT= process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
+var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
@@ -21,13 +23,13 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgerBoss.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-//synchronize sequelize and listen on port 3000
-
+// Syncing our sequelize models and then starting our express app
 db.sequelize.sync({ force: true }).then(function() {
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
